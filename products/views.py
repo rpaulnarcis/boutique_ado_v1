19f995d14edaf25917713ffb59663985dc5a9b3a
rpaulnarcis <rpaulnarcis@gmail.com>
2021-04-25 08:06:39 +0000

    Updated allauth templates and tested profiles

--- products/views.py
+++ products/views.py
@@ -23,7 +23,7 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
-                sortkey = 'category__name'    
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -65,4 +65,4 @@ def product_detail(request, product_id):
         'product': product,
     }
     
-    return render(request, 'products/product_detail.html', context)    
+    return render(request, 'products/product_detail.html', context)
